a039c642717783ba2cb33ec4ec6d9d8315a65dc0
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-11-16 15:42:16 +0400

    Initial commit

---
+++ ecomsite/shop/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Products, Order
+
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category = "default")
+
+    change_category_to_default.short_description = "Default Category"
+    list_display = ('title', 'price', 'discount_price', 'category', 'description', 'image')
+    search_fields = ('title',)
+    actions = {'change_category_to_default',}
+    list_editable = ('price', 'category',)
+
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Order)
